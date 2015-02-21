ae99a05a5584d76628177ecb505a1c515e73db5b
dylan.chen <dylan_zp_chen@163.com>
2015-02-21 14:28:17 +0800

    修改 开发手册 修改 代码生成插件的版本 去除 部分无用代码

--- src/main/java/org/xllapp/portal/demo/controller/DemoController.java
+++ src/main/java/org/xllapp/portal/demo/controller/DemoController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 import org.xllapp.mvc.controller.SimpleController;
 import org.xllapp.mvc.dao.CRUDDao;
